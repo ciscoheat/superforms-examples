ca565c36236b3c202b02d9cfaf64d471285efd71
Andreas Söderlund <ciscoheat@gmail.com>
2024-03-13 07:41:15 +0100

    batch file update

--- new-lib.bat
+++ new-lib.bat
@@ -1,9 +1,9 @@
 IF [%1] == [] GOTO :end
-add-example %1
+call add-example %1
 sed -i "s#zod#%1#g" src\routes\+page.server.ts
 sed -i -r "s#- Zod#- %1#" src\routes\+page.svelte
 sed -i -r "s#(testing ground - )(\w)#\1\u\2#" src\routes\+page.svelte
-pnpm remove zod
 pnpm i %1
+pnpm remove zod
 
 :end
