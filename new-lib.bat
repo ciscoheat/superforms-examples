ff2f6057a5969386ef26ba1eb34e1afd85eeece1
Andreas Söderlund <ciscoheat@gmail.com>
2024-03-13 07:46:58 +0100

    batch file again

--- new-lib.bat
+++ new-lib.bat
@@ -3,7 +3,7 @@ call add-example %1
 sed -i "s#zod#%1#g" src\routes\+page.server.ts
 sed -i -r "s#- Zod#- %1#" src\routes\+page.svelte
 sed -i -r "s#(testing ground - )(\w)#\1\u\2#" src\routes\+page.svelte
-pnpm i %1
-pnpm remove zod
+cmd /c pnpm remove zod
+cmd /c pnpm i %1
 
 :end
