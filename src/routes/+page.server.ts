4e82ee848c7bf41a5b20da5dbe37b04d235c6bab
Andreas Söderlund <ciscoheat@gmail.com>
2024-03-13 07:40:42 +0100

    Arktype

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,17 +1,20 @@
 import type { Actions, PageServerLoad } from './$types.js';
 
 import { superValidate, message } from 'sveltekit-superforms';
-import { zod } from 'sveltekit-superforms/adapters';
+import { arktype } from 'sveltekit-superforms/adapters';
 import { fail } from '@sveltejs/kit';
 import { schema } from './schema.js';
 
+// Arktype requires explicit default values for now
+const defaults = { name: 'Hello world!', email: '' };
+
 export const load: PageServerLoad = async () => {
-	return { form: await superValidate(zod(schema)) };
+	return { form: await superValidate(arktype(schema, { defaults })) };
 };
 
 export const actions: Actions = {
 	default: async ({ request }) => {
-		const form = await superValidate(request, zod(schema));
+		const form = await superValidate(request, arktype(schema, { defaults }));
 		console.log(form);
 
 		if (!form.valid) return fail(400, { form });
