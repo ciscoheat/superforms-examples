4878316e0503d12dfd2416288ff72729fc4bc97f
Andreas Söderlund <ciscoheat@gmail.com>
2024-03-13 08:07:54 +0100

    Yup

--- src/routes/schema.ts
+++ src/routes/schema.ts
@@ -1,7 +1,7 @@
-import { z } from 'zod';
+import { object, string } from 'yup';
 
 // Define at the top-level so it stays in memory and the adapter can be cached
-export const schema = z.object({
-	name: z.string().default('Hello world!'),
-	email: z.string().email()
+export const schema = object({
+	name: string().default('Hello world!'),
+	email: string().email().required()
 });
