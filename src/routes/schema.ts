4cb6d32a8612bf50125a0b78e3a00394ffb8fec7
Andreas Söderlund <ciscoheat@gmail.com>
2024-03-13 08:20:34 +0100

    Arktype

--- src/routes/schema.ts
+++ src/routes/schema.ts
@@ -1,7 +1,6 @@
-import { z } from 'zod';
+import { type } from 'arktype';
 
-// Define at the top-level so it stays in memory and the adapter can be cached
-export const schema = z.object({
-	name: z.string().default('Hello world!'),
-	email: z.string().email()
+export const schema = type({
+	name: 'string',
+	email: 'email'
 });
