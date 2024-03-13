0847deceb880e10bfdad07f7769d26840f8695d1
Andreas Söderlund <ciscoheat@gmail.com>
2024-03-13 07:52:07 +0100

    VineJS

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,17 +1,20 @@
 import type { Actions, PageServerLoad } from './$types.js';
 
 import { superValidate, message } from 'sveltekit-superforms';
-import { zod } from 'sveltekit-superforms/adapters';
+import { vine } from 'sveltekit-superforms/adapters';
 import { fail } from '@sveltejs/kit';
 import { schema } from './schema.js';
 
+// VineJS requires explicit default values
+const defaults = { name: 'Hello world!', email: '' };
+
 export const load: PageServerLoad = async () => {
-	return { form: await superValidate(zod(schema)) };
+	return { form: await superValidate(vine(schema, { defaults })) };
 };
 
 export const actions: Actions = {
 	default: async ({ request }) => {
-		const form = await superValidate(request, zod(schema));
+		const form = await superValidate(request, vine(schema, { defaults }));
 		console.log(form);
 
 		if (!form.valid) return fail(400, { form });
