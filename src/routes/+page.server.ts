29d4dee2be8e72f008234cd400fcc0636f215c61
Andreas Söderlund <ciscoheat@gmail.com>
2024-03-14 16:40:31 +0100

    Added

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,21 +1,25 @@
 import type { Actions, PageServerLoad } from './$types.js';
-
 import { superValidate, message } from 'sveltekit-superforms';
 import { valibot } from 'sveltekit-superforms/adapters';
 import { fail } from '@sveltejs/kit';
-import { schema } from './schema.js';
+import { schema } from './schema';
 
 export const load: PageServerLoad = async () => {
-	return { form: await superValidate(valibot(schema)) };
+	const form = await superValidate(valibot(schema));
+	return { form };
 };
 
-export const actions: Actions = {
+export const actions = {
 	default: async ({ request }) => {
 		const form = await superValidate(request, valibot(schema));
-		console.log(form);
+
+		console.log('POST', form);
 
 		if (!form.valid) return fail(400, { form });
 
-		return message(form, 'Form posted successfully!');
+		return message(
+			form,
+			'An AI will review your information and get back to you soon. Thank you for being a cog in a fully automated machine.'
+		);
 	}
-};
+} satisfies Actions;
