cf050b49546a9107e661f6915b92b47143e00527
Andreas Söderlund <ciscoheat@gmail.com>
2024-03-14 17:31:47 +0100

    Added

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,21 +1,43 @@
 import type { Actions, PageServerLoad } from './$types.js';
-
 import { superValidate, message } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
-import { fail } from '@sveltejs/kit';
-import { schema } from './schema.js';
+import { z } from 'zod';
+import { constellations, constellationSchema } from '$lib/constellations';
+
+const schema = z.object({
+	id: constellationSchema.shape.id
+});
 
 export const load: PageServerLoad = async () => {
-	return { form: await superValidate(zod(schema)) };
+	const form = await superValidate(zod(schema));
+	return { form, constellations };
 };
 
 export const actions: Actions = {
 	default: async ({ request }) => {
+		await new Promise((r) => setTimeout(r, 1000));
+
 		const form = await superValidate(request, zod(schema));
-		console.log(form);
 
-		if (!form.valid) return fail(400, { form });
+		console.log('POST', form);
 
-		return message(form, 'Form posted successfully!');
+		if (!form.valid) {
+			return message(form, 'Invalid ID for constellation.');
+		}
+
+		const index = constellations.findIndex((s) => s.id == form.data.id);
+
+		if (!constellations[index]) {
+			return message(form, 'Cannot find constellation in database.', { status: 404 });
+		}
+
+		if (constellations[index].locked) {
+			return message(form, 'Constellation is locked, could not delete.', { status: 403 });
+		}
+
+		// Delete from DB
+		constellations.splice(index, 1);
+
+		return message(form, 'Constellation deleted from database.');
 	}
 };
