dfe14cee5b9e2dcacfc4ee5494f9a461078f6fcd
Andreas Söderlund <ciscoheat@gmail.com>
2024-03-14 17:44:04 +0100

    Added

--- src/routes/schema.ts
+++ src/routes/schema.ts
@@ -1,6 +1,9 @@
 import { z } from 'zod';
 
 export const schema = z.object({
-	name: z.string().min(2),
-	email: z.string().email()
+	email: z.string().email(),
+	username: z
+		.string()
+		.min(3, 'Must be at least three characters.')
+		.regex(/^[a-z]+$/, 'Must use only a-z characters.')
 });
