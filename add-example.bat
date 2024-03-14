db4c51ee507ffd48c54a72dabe5f4988c1409424
Andreas Söderlund <ciscoheat@gmail.com>
2024-03-14 18:07:56 +0100

    Removed batch files

--- add-example.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-IF [%1] == [] GOTO :end
-IF [%2] == [] (SET FROM=main) ELSE (SET FROM=%2)
-
-git checkout -b %1 %FROM%
-::git push -u origin %1
-
-:end
--- new-lib.bat
+++
@@ -1,9 +0,0 @@
-IF [%1] == [] GOTO :end
-call add-example %1
-sed -i "s#zod#%1#g" src\routes\+page.server.ts
-sed -i -r "s#- Zod#- %1#" src\routes\+page.svelte
-sed -i -r "s#(testing ground - )(\w)#\1\u\2#" src\routes\+page.svelte
-cmd /c pnpm remove zod
-cmd /c pnpm i %1
-
-:end
