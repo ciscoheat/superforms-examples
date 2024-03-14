cfef97a3f3d746ab600e71914f54241a876f18c1
Andreas Söderlund <ciscoheat@gmail.com>
2024-03-14 17:05:23 +0100

    Added

--- src/routes/schema.ts
+++ src/routes/schema.ts
@@ -1,6 +1,15 @@
 import { z } from 'zod';
 
-export const schema = z.object({
-	name: z.string().min(2),
-	email: z.string().email()
-});
+export const schema = z
+	.object({
+		scoops: z.number().int().min(1).default(1),
+		flavours: z
+			.string()
+			.array()
+			.min(1, 'Please select at least one flavour')
+			.default(['Mint choc chip'])
+	})
+	.refine((data) => data.flavours.length <= data.scoops, {
+		message: "Can't order more flavours than scoops!",
+		path: ['flavours']
+	});
