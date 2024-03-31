5ce77b40c0655ebc6bfb3dde025e8f0649deaec2
Andreas Söderlund <ciscoheat@gmail.com>
2024-03-31 03:51:34 +0200

    Updated with unlock button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -49,14 +49,31 @@
 					<td>{c.locked ? 'Yes' : 'No'}</td>
 					<td class="has-spinner">
 						<button
+							formaction="?/delete"
 							name="id"
 							value={c.id}
 							class="delete"
-							on:click={() => ($formId = c.id.toString())}>Delete</button
+							on:click={() => ($formId = c.id.toString())}
 						>
-
-						{#if $delayed && $formId == c.id.toString()}
-							<img alt="Loading..." class="spinner" src={spinner} />
+							{#if $delayed && $formId == c.id.toString()}
+								<img alt="Loading..." class="spinner" src={spinner} />
+							{:else}
+								Delete
+							{/if}
+						</button>
+						{#if c.locked}
+							<button
+								formaction="?/unlock"
+								name="id"
+								value={c.id}
+								on:click={() => ($formId = c.id.toString())}
+							>
+								{#if $delayed && $formId == c.id.toString()}
+									<img alt="Loading..." class="spinner" src={spinner} />
+								{:else}
+									Unlock
+								{/if}
+							</button>
 						{/if}
 					</td>
 				</tr>
@@ -66,6 +83,10 @@
 </form>
 
 <style>
+	button {
+		min-width: 75px;
+	}
+
 	a {
 		text-decoration: underline;
 	}
