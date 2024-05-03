34a9fd8f8bf8d20d6e38f14a27bbbfe7741986b3
Andreas Söderlund <ciscoheat@gmail.com>
2024-05-03 10:49:28 +0200

    Tutorial for JSON Schema

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,11 @@
 import { message, superValidate } from 'sveltekit-superforms';
-import { zod } from 'sveltekit-superforms/adapters';
+import { schemasafe } from 'sveltekit-superforms/adapters';
 import { fail } from '@sveltejs/kit';
 import { schema } from './schema.js';
 
 export const load = async () => {
-	const form = await superValidate(zod(schema));
+	const adapter = schemasafe(schema);
+	const form = await superValidate(adapter);
 
 	// Always return { form } in load functions
 	return { form };
@@ -12,7 +13,9 @@ export const load = async () => {
 
 export const actions = {
 	default: async ({ request }) => {
-		const form = await superValidate(request, zod(schema));
+		const adapter = schemasafe(schema);
+		const form = await superValidate(request, adapter);
+
 		console.log(form);
 
 		if (!form.valid) {
