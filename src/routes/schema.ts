ea379f2f53e12039c0ed0ddb8949b13532170a57
Andreas Söderlund <ciscoheat@gmail.com>
2024-05-10 09:12:57 +0200

    Min length for worker name

--- src/routes/schema.ts
+++ src/routes/schema.ts
@@ -4,7 +4,7 @@ export const schema = z.object({
 	workDone: z
 		.object({
 			date: z.date().nullish(),
-			worker: z.string().nullish()
+			worker: z.string().min(1).nullish()
 		})
 		.array()
 });
