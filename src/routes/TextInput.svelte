375824ab452145b2e1777dc35a9fdee401b1a5c5
Andreas Söderlund <ciscoheat@gmail.com>
2024-05-10 09:02:15 +0200

    Add worker button Text input component

---
+++ src/routes/TextInput.svelte
@@ -0,0 +1,23 @@
+<script lang="ts" context="module">
+	type T = Record<string, unknown>;
+</script>
+
+<script lang="ts" generics="T extends Record<string, unknown>">
+	import { formFieldProxy, type SuperForm, type FormPathLeaves } from 'sveltekit-superforms';
+
+	export let superform: SuperForm<T>;
+	export let field: FormPathLeaves<T, string>;
+
+	const { value, errors } = formFieldProxy(superform, field);
+</script>
+
+<input aria-invalid={$errors ? 'true' : undefined} bind:value={$value} />
+{#if $errors}
+	<span class="invalid">{$errors}</span>
+{/if}
+
+<style>
+	.invalid {
+		color: red;
+	}
+</style>
