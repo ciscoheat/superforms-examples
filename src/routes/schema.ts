8679b4d37b63922a5d0f7886cab42d769f4fcf36
Andreas Söderlund <ciscoheat@gmail.com>
2024-06-10 12:27:10 +0200

    Valibot updated to 0.31

--- src/routes/schema.ts
+++ src/routes/schema.ts
@@ -1,7 +1,7 @@
-import { object, string, number, integer, minLength, minValue, email, maxValue } from 'valibot';
+import { pipe, object, string, number, integer, minLength, minValue, email, maxValue } from 'valibot';
 
 export const schema = object({
-	name: string([minLength(3, 'Too short')]),
-	points: number([integer(), minValue(0), maxValue(5)]),
-	email: string([email()])
+	name: pipe(string(),minLength(3, 'Too short')),
+	points: pipe(number(), integer(), minValue(0), maxValue(5)),
+	email: pipe(string(), email())
 });
