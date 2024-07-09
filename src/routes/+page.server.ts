ad1e9f5864dfc7d99e1c4ce738c831a671be3638
Andreas Söderlund <ciscoheat@gmail.com>
2024-07-10 00:10:06 +0200

    Superstruct

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,17 +1,19 @@
 import type { Actions, PageServerLoad } from './$types.js';
 
 import { superValidate, message } from 'sveltekit-superforms';
-import { zod } from 'sveltekit-superforms/adapters';
+import { superstruct } from 'sveltekit-superforms/adapters';
 import { fail } from '@sveltejs/kit';
 import { schema } from './schema.js';
 
+const defaults = { name: '', email: '' };
+
 export const load: PageServerLoad = async () => {
-	return { form: await superValidate(zod(schema)) };
+	return { form: await superValidate(superstruct(schema, { defaults })) };
 };
 
 export const actions: Actions = {
 	default: async ({ request }) => {
-		const form = await superValidate(request, zod(schema));
+		const form = await superValidate(request, superstruct(schema, { defaults }));
 		console.log(form);
 
 		if (!form.valid) return fail(400, { form });
