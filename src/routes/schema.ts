9f787959b80665457bc8d430a311ec44efd326da
Andreas Söderlund <ciscoheat@gmail.com>
2025-02-05 18:40:10 +0100

    Discriminated union

--- src/routes/schema.ts
+++ src/routes/schema.ts
@@ -1,6 +1,11 @@
 import { z } from 'zod';
 
-export const schema = z.object({
-	name: z.string().min(2),
-	email: z.string().email()
-});
+export const schema = z.discriminatedUnion('type', [
+	z.object({
+		type: z.literal('empty')
+	}),
+	z.object({
+		type: z.literal('extra'),
+		name: z.string().min(1)
+	})
+]);
